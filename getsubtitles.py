8b93f2987ce3c67347e2b0b019b63886932b3709
Sicco van Sas <sicco@ddo.nl>
2013-11-10 14:37:33 +0100

    changed folder name to kro-subtitles, as this is used in the rest of the code.

--- getsubtitles.py
+++ getsubtitles.py
@@ -4,5 +4,5 @@
 
 # Dit is nog geen scraper ^^ ^^.
 import os
-os.system("mkdir subtitles")
+os.system("mkdir kro-subtitles")
 os.system("curl -0 http://www.hackdeoverheid.nl/wp-content/uploads/sites/10/2013/11/kro-subtitles.zip | tar -zx -C subtitles")

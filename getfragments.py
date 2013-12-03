b0f820fee13dc0e9e72551c202393868ec50946d
Maarten van der Velden <maarten.vdvelden@gmail.com>
2013-12-03 19:50:56 +0100

    Main file, times duration, new paths

--- getfragments.py
+++ getfragments.py
@@ -1,17 +1,21 @@
 #! /usr/bin/python
 
+
+# TODO: print duration, select search strategy (chronologically), bool shuffle results, store t888 & program info in a database
 import json
 import os
 import re
 import sys
 import pprint
 import random
+import time
 
-usage = """usage: ./get_kro_subs.py SEARCH_REGEX [PROGRAMMA_REGEX output.txt]"""
+usage = """usage: ./getfragments.py SEARCH_REGEX [PROGRAMMA_REGEX Shuffle output.txt]"""
 
+start_time = time.time()
 programmaregex = ".*"
 outfile = 'output.txt'
-topx = 1
+shuffle = False
 if len(sys.argv) == 1:
     print usage
     sys.exit()
@@ -20,13 +24,15 @@ if len(sys.argv) > 1:
 if len(sys.argv) > 2:
     programmaregex = sys.argv[2]
 if len(sys.argv) > 3:
-    outfile = sys.argv[2]
+    if sys.argv[3] == '1' or sys.argv[3] == 'True':
+        shuffle = True
+if len(sys.argv) > 4:
+    outfile = sys.argv[4]
 # programmanaam = programmanaam.lower()
 # searchstring = searchstring.lower()
 
-subtitlefolder = 'kro-subtitles/subtitles/'
-pjsonfolder = 'kro-subtitles/program_info'
-
+subtitlefolder = 'subtitles/'
+pjsonfolder = 'program_info/'
 
 # Ga programma's af
 frequencydict = {}
@@ -51,9 +57,12 @@ for filename in os.listdir(subtitlefolder):
             prid = x['prid']
         if 'titel' in x:
             ptitel = x['titel']
-            
+            pgidsdatum = 'onbekend'
+            if 'gidsdatum' in x:
+                pgidsdatum = x['gidsdatum']
             if re.search(programmaregex, ptitel, re.IGNORECASE):
                 # Zoek in goede programma
+                print filename, ptitel,  pgidsdatum
                 prevline = ''
                 for line in file:
                     if not prevline == '' and re.search(searchregex, line):
@@ -64,13 +73,13 @@ for filename in os.listdir(subtitlefolder):
                         starttime = prevsplit[0]
                         try:
                             endtime = prevsplit[2].strip()
+                            print '  ' + line,
+                            frequencydict[prid].append([prid, starttime, endtime, line.strip()])
                         except:
-                            print '<EXCEPTION>'
-                            endtime = ""
+                            print '  <EXCEPTION>: Unable to read times from string "%s"' %(prevsplit, )
                         # print filename
                         # print prevline,
-                        print line,
-                        frequencydict[prid].append([prid, starttime, endtime, line.strip()])
+
                         #print jfile
                         #js = json.loads(jfile)
                     prevline = line
@@ -84,23 +93,36 @@ sorted_prid = sorted(frequencydict, key=lambda k: len(frequencydict[k]), reverse
 # sorted_prid = sorted_prid[0:1]
 print len(sorted_prid)
 # print open(pjsonfolder + '/' + sorted_prid[0] + '.json').read()
-tmpfile = 'tmp'
-with open(tmpfile,'w') as resfile:
+if shuffle:
+    tempfile = outfile
+    outfile = '.'.join(outfile.split('.')[:-1] + ['tmp'])
+with open(outfile,'w') as resfile:
     for pr in sorted_prid:
         for occurrences in frequencydict[pr]:
             resfile.write(repr(occurrences))
             resfile.write('\n')
-with open(outfile, 'w') as resfile:
-    with open(tmpfile, 'r') as tmp:
-        lines = tmp.readlines()
-        random.shuffle(lines)
-        for line in lines:
-            resfile.write(line)
-        # for fragment in frequencydict[pr]:
-        #     resfile.write(fragment)
+if shuffle:
+    with open(tmpfile, 'w') as resfile:
+        with open(outfile, 'r') as tmp:
+            lines = tmp.readlines()
+            random.shuffle(lines)
+            for line in lines:
+                resfile.write(line)
+print "Finished in %.0f seconds" % (time.time() - start_time)
 # if 'titel' in x:
 #     print x['titel']
 #     if not x['titel'] in programmadict:
 #         programmadict[x['titel']] = 0
 #     programmadict[x['titel']] += 1
 # pprint.pprint(programmadict)
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description='Query the T888 database to find fragments, and build a fragments file from it.')
+#     parser.add_argument('search_regex', type=argparse.String, help='search query')
+#     parser.add_argument('program_regex', type=argpase.String, help='define in which tv program to search', default='.*')
+#     parser.add_argument('output_file', type=argparse.FileType('w'), help='file to save fragments to', default='output.txt')
+#     parser.add_argument('t888_folder', type=argparse.String, help='folder where to search for subtitle files', default='subtitles')
+#     
+#     args = parser.parse_args()
+# 
+#     main(args.fragments)

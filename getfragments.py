b7657f58c89153a6765ccb9e25aecc77ad74c701
Maarten van der Velden <maarten.vdvelden@gmail.com>
2013-12-03 22:04:38 +0100

    More cleaning Export to json

--- getfragments.py
+++ getfragments.py
@@ -10,6 +10,64 @@ import pprint
 import random
 import time
 import argparse
+from datetime import timedelta
+
+
+def parsetimedelta(timestring):
+    d = timedelta(hours=int(timestring[0:2]), minutes=int(timestring[3:5]), 
+        seconds=int(timestring[6:8]), milliseconds=int(timestring[9:12]))
+    return d
+
+def printtimedelta(td):
+    return '%02d:%02d:%02d.%03d' % (td.seconds//3600, (td.seconds//60) % 60, td.seconds % 60, td.microseconds/1000)
+
+
+def convert_to_json(jsonfile):
+    # read json file
+    jfile = jsonfile.read().split('\n')[0]
+    #het is jsonp, dus haal functiecall eraf
+    jfile = jfile[14:-2]
+    # probeer te laden
+    try:
+        return json.loads(jfile)
+    except:
+        print 'invalid json: ' + filename
+        return None
+
+def parse_subtitles(programma_json, search_regex, subsfilename, offset=0.0):
+    
+    # get programma id
+    prid = 'undefined'
+    if 'prid' in programma_json:
+        prid = programma_json['prid']
+    pgidsdatum = 'onbekend'
+    if 'gidsdatum' in programma_json:
+        pgidsdatum = programma_json['gidsdatum']
+    
+    print subsfilename, programma_json['titel'],  pgidsdatum
+    
+    # Walk the subsfile
+    prevline = ''
+    
+    result = []
+    
+    with open(subsfilename, 'r') as subsfile:
+        for line in subsfile:
+            # Previous line cannot be empty (should have subtitle timing data), and regex should match
+            if not prevline == '' and re.search(search_regex, line):
+                # get start and endtime of subtitle
+                prevlinesplit = prevline.split(' ')
+                starttime = prevlinesplit[0]
+                try:
+                    endtime = prevlinesplit[2].strip()
+                    print '  ' + line,
+                    result.append({'prid':prid, 'start_time':starttime, 'end_time':endtime, 'text':line.strip()})
+                except:
+                    print '  <EXCEPTION>: Unable to read times from string "%s"' %(prevsplit, )
+            prevline = line
+    # returns a list of dicts, each element is a matching line, each dict has keys 'start_time' 'end_time' 'text'
+    return result
+
 
 def main(arguments):
     
@@ -24,87 +82,37 @@ def main(arguments):
     print "Finding subtitles with expression: %s, in programs with expression %s\nfrom source '%s' to output file '%s'"%(search_regex, program_regex, subs_folder, outfile.name)
     
     # Ga programma's af
-    frequencydict = {}
+    resultlist = []
 
     for filename in os.listdir(subs_folder):
-        if not filename[0] == ".":
-            # open subs
-            file = open(subs_folder + '/' + filename)
-            # open json file
-            jfile = open(program_info + '/' + filename.split('.')[0] + '.json').read().split('\n')[0]
-            #het is jsonp, dus haal functiecall eraf
-            jfile = jfile[14:-2]
-            # probeer te laden
-            try:
-                x = json.loads(jfile)
-            except:
-                print 'invalid json: ' + filename
-                continue
-            # get programma id
-            prid = 'undefined'
-            if 'prid' in x:
-                prid = x['prid']
-            if 'titel' in x:
-                ptitel = x['titel']
-                pgidsdatum = 'onbekend'
-                if 'gidsdatum' in x:
-                    pgidsdatum = x['gidsdatum']
-                if re.search(program_regex, ptitel, re.IGNORECASE):
-                    # Zoek in goede programma
-                    print filename, ptitel,  pgidsdatum
-                    prevline = ''
-                    for line in file:
-                        if not prevline == '' and re.search(search_regex, line):
-                            if not prid in frequencydict:
-                                frequencydict[prid] = []
-                            # get start en endtime of subtitle
-                            prevsplit = prevline.split(' ')
-                            starttime = prevsplit[0]
-                            try:
-                                endtime = prevsplit[2].strip()
-                                print '  ' + line,
-                                frequencydict[prid].append([prid, starttime, endtime, line.strip()])
-                            except:
-                                print '  <EXCEPTION>: Unable to read times from string "%s"' %(prevsplit, )
-                            # print filename
-                            # print prevline,
-
-                            #print jfile
-                            #js = json.loads(jfile)
-                        prevline = line
-
-    sorted_prid = sorted(frequencydict, key=lambda k: len(frequencydict[k]), reverse=True)
-    # if topx > 0 and topx < len(frequencydict.items()):
-    #     print sorted_prid[:topx]
-    # else :
-        # topx = len(frequencydict.items())
-        # print sorted_prid
-    # sorted_prid = sorted_prid[0:1]
-    print len(sorted_prid)
-    # print open(program_info + '/' + sorted_prid[0] + '.json').read()
+        programma_json = None
+        with open(os.path.join(program_info, filename.split('.')[0] + '.json'), 'r') as jsonfile:
+            programma_json = convert_to_json(jsonfile)
+        
+        if not programma_json is None:
+            ptitel = 'onbekend'
+            if 'titel' in programma_json:
+                ptitel = programma_json['titel']
+            # Ignore case, because messy data
+            if re.search(program_regex, ptitel, re.IGNORECASE):
+                result = parse_subtitles(programma_json, search_regex, os.path.join(subs_folder, filename))
+                if not len(result) == 0:
+                    resultlist.extend(result)
+    
+    # sorted_prid = sorted(frequencydict, key=lambda k: len(frequencydict[k]), reverse=True)
     if shuffle:
-        tempfile = outfile
-        outfile = '.'.join(outfile.split('.')[:-1] + ['tmp'])
-    with open(outfile,'w') as resfile:
-        for pr in sorted_prid:
-            for occurrences in frequencydict[pr]:
-                resfile.write(repr(occurrences))
-                resfile.write('\n')
-    if shuffle:
-        with open(tmpfile, 'w') as resfile:
-            with open(outfile, 'r') as tmp:
-                lines = tmp.readlines()
-                random.shuffle(lines)
-                for line in lines:
-                    resfile.write(line)
+        random.shuffle(resultlist)
+    
+    json.dump(resultlist, outfile)
+    
     print "Finished in %.0f seconds" % (time.time() - start_time)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Query the T888 database to find fragments, and build a fragments file from it.')
     parser.add_argument('search_regex', type=str, help='search query')
+    parser.add_argument('output', type=argparse.FileType('w'), help='file to save fragments to')
     parser.add_argument('--program_regex', type=str, help='define in which tv program to search', default='.*')
-    parser.add_argument('--output', type=argparse.FileType('w'), help='file to save fragments to', default='fragments/output.txt')
     parser.add_argument('--subs', type=str, help='folder where to search for subtitle files', default='subtitles')
     parser.add_argument('--program_info', type=str, help='folder where to search for program info', default='program_info')
     parser.add_argument('--shuffle', type=bool, default=False, help='toggle shuffling, default is False')
